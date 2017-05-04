3f7ade0e5608ac0b1e9414ff529d73e8a87194c1
Kazuya <kazuya.cc@gmail.com>
2017-05-04 17:32:09 +0800

    2017/05/04 Add Proxy & Http Map
    2017/05/04 Add Proxy & Http Map

--- httpDemo/src/app/app.component.ts
+++ httpDemo/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Pm25Service } from './pm25.service';
-import { Http,  Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Pm25Item } from './pm25-item';
-import { Component, OnInit} from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +10,24 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app works!';
+  title = 'http app works!';
 
   items: Pm25Item[] = [];
 
-  constructor(private pmservice: Pm25Service) {}
+  data: any;
+
+  constructor(private pmservice: Pm25Service, private http: Http) { }
 
   ngOnInit() {
-    this.pmservice.loadItems().subscribe((response: Response) => {
-      this.items = response.json();
-    });
+    // this.pmservice.loadItems().subscribe((response: Response) => {
+    //   this.items = response.json();
+    let url = '/ws/Data/REWXQA/?%24orderby=SiteName&%24skip=0&%24top=1000&format=json';
+    this.data = this.http.get(url).map(x => x.json);
+
+    // this.http.get('/ws/Data/REWXQA/?%24orderby=SiteName&%24skip=0&%24top=1000&format=json').subscribe(response => {
+    //   this.data = response.json();
+    // });
+
+     console.log("hava data ?" , this.data);
   }
 }
