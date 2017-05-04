116dda1a8745cc111e679651a962e14d01217cef
Kazuya <kazuya.cc@gmail.com>
2017-05-04 14:48:47 +0800

    2017/05/04 Add Interface
    2017/05/04 Add Interface

--- httpDemo/src/app/app.component.ts
+++ httpDemo/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  items: TodoItem[] = [];
+
+  loadTodoItems(){
+    let request = this.http.get('assets/data.json');
+    request.subscribe(response =>{
+      this.items = response.json();
+    });
+
+    return request;
+  }
+
 }
